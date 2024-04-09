0286df1e44616c6f5db693eca15ab763fc73e4dc
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-04-09 16:33:26 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <h1>BIENVENIDOS A PROPIEDADES Y DISEÑO!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
