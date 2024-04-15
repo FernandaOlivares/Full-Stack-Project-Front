abe86b22dbc41890b5e959121741517b094d334b
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-04-15 17:12:58 -0400

    getAllProperties HOME

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,15 +1,12 @@
 import {Link} from 'react-router-dom';
 import styles from './NavBar.module.css';
-import logoPyd from '../../assets/logoPyd.jpg';
 
 function NavBar() {
 
     return (
       <>
         <div className={styles.navBarContainer}>
-        <img src={logoPyd} alt="logoPyd" className={styles.logoPyd} />
-        <Link className={styles.homeButton} to= '/home'>Home</Link>
-        <Link className={styles.publicarButton} to= '/create'>Publicar Propiedad</Link>
+          <h2>Nav Bar Para Filtros</h2>
         </div>
       </>
     )
